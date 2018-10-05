690c46d01e12dc0db373df4862ba30f4fc3099c5
Tetiana <tanyaagolovina@gmail.com>
2018-10-05 16:58:39 +0300

    Upgraded DecimalNumbersModel

--- DecimalNumbers/src/com.tg.decimalnumbersmodel/DecimalNumbersModel.java
+++ DecimalNumbers/src/com.tg.decimalnumbersmodel/DecimalNumbersModel.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DecimalNumbersModel {
     private int number;
-    private int n;
+    private int system;
     private String newNumber;
     private String revertedNumber;
     public String hexadecimalNumb;
@@ -15,20 +15,20 @@ public class DecimalNumbersModel {
     }
     public void setSystem(){
         Scanner in = new Scanner(System.in);
-        n = in.nextInt();
+        system = in.nextInt();
     }
     public void translateNumber(){
         newNumber = "";
         hexadecimalNumb = "0123456789ABCDEF";
         do{
-            newNumber += number%n;
-            number = number/n;
+            newNumber += number%system;
+            number = number/system;
         }
-        while(n<=number);
+        while(system<=number);
         if(number>9){
-            newNumber += hexadecimalNumb.charAt(number%n);
+            newNumber += hexadecimalNumb.charAt(number%system);
         } else {
-            newNumber += number%n;
+            newNumber += number%system;
         }
     }
     public void revert(){
