5ed17f560ca6c68e221ce66b5ecd427e94cb0169
Tetiana <tanyaagolovina@gmail.com>
2018-11-25 00:54:08 +0200

    Some changes added to Project2

--- Project2/src/controller/command/Macros.java
+++ Project2/src/controller/command/Macros.java
@@ -14,29 +14,14 @@ public class Macros implements Command {
     @Override
     public void execute(Text text) {
         CommandManager commandManager = defineMenuOperation(input-1);
-        switch (commandManager) {
-            case READ_FILE:
-                commandManager.getCommand().execute(text);
-                break;
-            case FIND_QUESTIONS:
-                commandManager.getCommand().execute(text);
-                break;
-            case FIND_WORDS:
-                commandManager.getCommand().execute(text);
-                break;
-            case DEFAULT:
-                commandManager.getCommand().execute(text);
-                break;
-            case EXIT:
-                commandManager.getCommand().execute(text);
-        }
+        commandManager.getCommand().execute(text);
     }
 
     private CommandManager defineMenuOperation(int number) {
-        CommandManager[] itemMenus = CommandManager.values();
-        if (number < 0 || number >= itemMenus.length) {
+        CommandManager[] itemsMenu = CommandManager.values();
+        if (number < 0 || number >= itemsMenu.length) {
             return CommandManager.DEFAULT;
         }
-        return itemMenus[number];
+        return itemsMenu[number];
     }
 }
